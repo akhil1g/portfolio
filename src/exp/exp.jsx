aa5c454a665201fe96707f1448e512e36dae40bf
Akhil Gupta <akhil.gupta.12817@gmail.com>
2024-04-13 00:02:42 +0530

    updatedExp

--- src/exp/exp.jsx
+++ src/exp/exp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './exp.css';
 import Sap from './saplabs.jfif';
 import MnnitEce from './mnnitece.webp'
+import Astro from './Capture.JPG'
 import { Reveal } from "../animations/Animations";
 export default function Exp()
 {
@@ -20,11 +21,11 @@ export default function Exp()
                     </div>
                 </div>
                 <div className="expbox exprightbox">
-                    <img className="expimg" src={Sap} alt=""></img>
+                    <img className="expimg" src={Astro} alt=""></img>
                     <div className="exptextbox">
-                        <h2>SAP Labs Inc.</h2>
-                        <small>May 2024-July 2024</small>
-                        <p>Software Developer Intern at SAP Labs. Not started</p>
+                        <h2>Astro Club MNNIT</h2>
+                        <small>Dec 2023-Present</small>
+                        <p>Club Coordinator of AstroWing MNNIT</p>
                         <span className="rightboxarrow"></span>
                     </div>
                 </div>
